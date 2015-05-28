00436cb1fb96be031f74a55fedf1b2d99a07ec04
LauraKirby <laurakirby26@gmail.com>
2015-05-28 16:08:46 -0700

    add instances of Paper.js classes Group and Layer. add additional shapes.

--- script.js
+++ script.js
@@ -5,25 +5,50 @@ $(document).ready(function(){
 		// Create an empty project and a view for the canvas:
 		paper.setup(canvas);
 		
+
 		// Create rectangle 
- 		var rect = new paper.Path.Rectangle(new paper.Point(0, 0),
-		new paper.Point(300, 300));
+ 		var rect = new paper.Path.Rectangle(new paper.Point(0, 0),(300, 300));
 		rect.fillColor = 'aqua'
 
-		// Create triangle 
+		// Create triangle - 3 arguments: (center, numSides, radius)
 		var triangle = new paper.Path.RegularPolygon(new paper.Point(200, 70), 3, 50);
 		triangle.fillColor = 'pink';
 
 		// Create circle
-		var circlePathGreen = new paper.Path.Circle(new paper.Point(50, 30), 20);
-		circlePathGreen.fillColor = 'green'
+		var circleGreen = new paper.Path.Circle(new paper.Point(50, 30), 20);
+		circleGreen.fillColor = 'green'
  
-		var circlePathPurp = new paper.Path.Circle(new paper.Point(50, 75), 20);
-		circlePathPurp.fillColor = 'purple'
+		var circlePurple = new paper.Path.Circle(new paper.Point(50, 75), 20);
+		circlePurple.fillColor = 'purple'
 
-		var circlePathOrange = new paper.Path.Circle(new paper.Point(50, 120), 20);
-		circlePathOrange.fillColor = 'orange'
+		var circleOrange = new paper.Path.Circle(new paper.Point(50, 120), 20);
+		circleOrange.fillColor = 'orange'
+
+		// Create an instance of Group for all background images
+		var backgroundGroup = new paper.Group([rect, triangle]); 
+
 		
+
+		// Create an instance of Group for all clickable images
+		var clickableGroup = new paper.Group([circleGreen, circlePurple, circleOrange]);
+
+		function onMouseDrag(event) {
+			
+		}
+
+			// Create a second layer by creating an instance of Layer 
+			var clickableImage = new paper.Layer();
+
+				// Add children to second layer
+				var rect = new paper.Path.Rectangle(new paper.Point(80, 60),50, 50);
+				rect.fillColor = 'grey'
+
+				var pentagonLeftEye = new paper.Path.RegularPolygon(new paper.Point(95, 75), 5, 7);
+				pentagonLeftEye.fillColor = '#FF33CC'; //bright pink
+
+				var pentagonRightEye = new paper.Path.RegularPolygon(new paper.Point(115, 75), 5, 7);
+				pentagonRightEye.fillColor = 'lime';
+
 
 
 		// Draw the view now:
