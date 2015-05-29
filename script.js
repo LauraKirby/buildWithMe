677801fab8238a71046e0714891ab9b7866b3576
LauraKirby <laurakirby26@gmail.com>
2015-05-29 10:31:09 -0700

    clarify comments for presentation

--- script.js
+++ script.js
@@ -2,12 +2,14 @@ $(document).ready(function(){
 		// Get a reference to the canvas object
 		var canvas = document.getElementById('myCanvas');
 		
-		// Create an empty project and a view for the canvas:
+		// Create an empty Project, this creates an instance of Project (project), View (view) and Layer (layer):
+		// paper is a global variable of the class Paper 
 		paper.setup(canvas);
 		
+		//SHAPES - implement Path & shape class 
 
 		// Create rectangle 
- 		var rect = new paper.Path.Rectangle(new paper.Point(0, 0),(300, 300));
+ 		var rect = new paper.Path.Rectangle(new paper.Point(0, 0),300, 300);
 		rect.fillColor = '#66FFFF' //aqua
 
 		// Create triangle - 3 arguments: (center, numSides, radius)
@@ -16,33 +18,38 @@ $(document).ready(function(){
 
 		// Create grey rectangle 
 		var rectGrey = new paper.Path.Rectangle(new paper.Point(80, 60),50, 30);
-		rectGrey.fillColor = 'grey'
+		rectGrey.fillColor = '#CCCCCC'
 
-		// Create pentagons
+		// Create pink pentagon
 		var pentagonLeftEye = new paper.Path.RegularPolygon(new paper.Point(95, 73), 5, 6);
 		pentagonLeftEye.fillColor = '#FF33CC'; //bright pink
 
+		// Create lime pentagon
 		var pentagonRightEye = new paper.Path.RegularPolygon(new paper.Point(115, 73), 5, 6);
 		pentagonRightEye.fillColor = 'lime';
 
-		// Create Layer
+		//LAYER
+		// Create second Layer, first layer was created upon project creation
+		// All items created after this layer will be on the second layer
 		var dragableLayer = new paper.Layer();
 		dragableLayer.name = "draggable";
 
-		// Create image on new layer 
+		// Create image on second layer 
 		var circleOrange = new paper.Path.Circle(new paper.Point(50, 120), 20);
 		circleOrange.fillColor = 'orange'
 
-		// Create a second instance of Group for robot head, the entire group of images is dragable. However, each item within the group is absolute. 
+		//GROUP
+		// Create an instance of Group for the robot head. The entire group of images is dragable. Each item within the group is absolute. 
 		var multiImage = new paper.Group([rectGrey, pentagonRightEye, pentagonLeftEye]);
 		multiImage.name = "robotHead"; 
 
- 		// Draw the view now:
+		//DRAW VIEW 
+ 		// Draw the view instance of View now:
 		paper.view.draw();
  
- 		// Create an instance of Tool for the mouse drage event
+ 		//TOOL 
+ 		// Create an instance of Tool for the mouse drag event
 		var tool = new paper.Tool();
-		//tool.activate(); this is already activated
  
  		// Create mouse drag 
 		tool.onMouseDrag = function(event){
@@ -61,5 +68,60 @@ $(document).ready(function(){
 	});
 
 
-		//TODO 
-		//create separate layer for each dragable image
+//TODO 
+//when cross over multiple images on same layer, keep current image clicked 
+//make project resizeable 
+//add images to create robot
+//education description of image draggeed
+
+//Attempting to abstract mouse events:
+
+// var hitOptions = {
+// 	segments: true,
+// 	stroke: true,
+// 	fill: true,
+// 	tolerance: 5
+// };
+
+//  		// Create an instance of Tool for the mouse drage event
+// 		var tool = new paper.Tool();
+
+// 			tool.onMouseDown = function(){
+// 			console.log("mouse down")
+// 			segment = path = null; 
+// 			var hitResult = paper.project.hitTest(event.point).item; 
+// 			if (!hitResult) {
+// 				return; 
+// 			}
+
+// 			if (paper.project.hitResult) {
+// 					path = paper.project.hitResult.item;
+// 					if (paper.project.hitResult.type == 'segment') {
+// 						segment = paper.project.hitResult.segment;
+// 					} 
+// 			}
+// 			// Add clicked item to active layer
+// 			movePath = paper.project.hitResult.type == 'fill';
+// 				if (movePath){
+// 					paper.project.activeLayer.addChild(paper.project.hitResult.item);
+// 					console.log(paper.project.hitResult.item);
+// 				}
+	
+// 		}
+ 		
+//  		tool.onMouseMove = function(event) {
+//  			paper.project.activeLayer.selected = false;
+//  			if (event.item)
+//  				event.item.selected = true;
+//  		}
+
+//  		tool.onMouseDrag = function(event) {
+//  			if (segment) {
+//  				segment.point += event.delta;
+//  				path.smooth();
+//  				console.log("segment")
+//  			} else if (path) {
+//  				path.position += event.delta;
+//  				console.log("path");
+//  			}
+//  		}
