6597989e77173eca29dc57936a507ec4fc0bbbb0
LauraKirby <laurakirby26@gmail.com>
2015-05-28 21:09:16 -0700

    add an instance of Group, a group of images can be dragged accross the view

--- script.js
+++ script.js
@@ -14,35 +14,28 @@ $(document).ready(function(){
 		var triangle = new paper.Path.RegularPolygon(new paper.Point(254, 122), 3, 53);
 		triangle.fillColor = '#CCFFFF'; //light aqua
 
+		// Create grey rectangle 
+		var rectGrey = new paper.Path.Rectangle(new paper.Point(80, 60),50, 30);
+		rectGrey.fillColor = 'grey'
 
-		// // Create grey rectangle
-		// var rectGrey = new paper.Path.Rectangle(new paper.Point(80, 60),50, 50);
-		// rectGrey.fillColor = 'grey'
+		// Create pentagons
+		var pentagonLeftEye = new paper.Path.RegularPolygon(new paper.Point(95, 73), 5, 6);
+		pentagonLeftEye.fillColor = '#FF33CC'; //bright pink
 
-		// // Create pentagons
-		// var pentagonLeftEye = new paper.Path.RegularPolygon(new paper.Point(95, 75), 5, 7);
-		// pentagonLeftEye.fillColor = '#FF33CC'; //bright pink
+		var pentagonRightEye = new paper.Path.RegularPolygon(new paper.Point(115, 73), 5, 6);
+		pentagonRightEye.fillColor = 'lime';
 
-		// var pentagonRightEye = new paper.Path.RegularPolygon(new paper.Point(115, 75), 5, 7);
-		// pentagonRightEye.fillColor = 'lime';
-
-
-
-
+		// Create Layer
 		var dragableLayer = new paper.Layer();
 		dragableLayer.name = "draggable";
 
+		// Create image on new layer 
 		var circleOrange = new paper.Path.Circle(new paper.Point(50, 120), 20);
 		circleOrange.fillColor = 'orange'
 
-		// Create an instance of Group for all background images
-		// Never move 
-		// var backgroundGroup = new paper.Group([rect, triangle]); 
-		
-		// Create a second instance of Group for robot head
-		//var dragableMultiImage = new paper.Group([rectGrey, pentagonRightEye, pentagonLeftEye]);
-				// Add children to dragableMultiImage group
-				
+		// Create a second instance of Group for robot head, the entire group of images is dragable. However, each item within the group is absolute. 
+		var multiImage = new paper.Group([rectGrey, pentagonRightEye, pentagonLeftEye]);
+		multiImage.name = "robotHead"; 
 
  		// Draw the view now:
 		paper.view.draw();
@@ -53,10 +46,20 @@ $(document).ready(function(){
  
  		// Create mouse drag 
 		tool.onMouseDrag = function(event){
-			var clickedItem = paper.project.hitTest(event.point); 
-			if(clickedItem && clickedItem.item.getLayer().name === "draggable"){
-	//			var mouseClicked = clickedItem.item;
-				clickedItem.item.position = event.point;
+			var clickedItem = paper.project.hitTest(event.point).item; 
+			if(clickedItem.getLayer().name === "draggable"){
+				if (clickedItem._parent.name === "robotHead")
+				{
+					clickedItem._parent.position = event.point;
+				}
+				else{
+					clickedItem.position = event.point;
+				}
 			}
 		}
+
 	});
+
+
+		//TODO 
+		//create separate layer for each dragable image
